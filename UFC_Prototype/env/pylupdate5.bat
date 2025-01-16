d9055a58617f798e1cd6ecc9f3e77e2a4931d66a
Cade <cadebrink2k@gmail.com>
2025-01-16 12:52:50 +0200

    feat: init

---
+++ UFC_Prototype/env/pylupdate5.bat
@@ -0,0 +1,1 @@
+@C:/Users/cadeb/Projects/UFC_Projects/UFC_Prototype/env\python.exe -m PyQt5.pylupdate_main %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ UFC_Prototype/env/pyuic5.bat
@@ -0,0 +1,1 @@
+@C:/Users/cadeb/Projects/UFC_Projects/UFC_Prototype/env\python.exe -m PyQt5.uic.pyuic %1 %2 %3 %4 %5 %6 %7 %8 %9
